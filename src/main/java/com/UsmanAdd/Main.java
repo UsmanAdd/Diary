a62689e13f33f428c99d244e243520d76aab06b0
Usman Adzhiev <34717949+usmanadd@users.noreply.github.com>
2020-06-21 18:51:40 +0300

    add file creating and writing

--- src/main/java/com/UsmanAdd/Main.java
+++ src/main/java/com/UsmanAdd/Main.java
@@ -1,12 +1,37 @@
 package com.UsmanAdd;
 
+
 public class Main {
 
+
     public static void main(String[] args) {
+        Input choose = new Input();
+        Tool tool = new Tool();
+
+        String answer = null;
+        String description;
+        String text;
         String type = "Консольное приложение";
         String version = "1.0.0";
 
         System.out.println("\t\tДобро пожаловать! В " + type + " версии " + version);
+
+        while (true) {
+            System.out.println("Список действий");
+            System.out.println("1) Добавить запись");
+            System.out.print("Выберите действие: ");
+            answer = choose.input();
+            switch (answer) {
+                case "1":
+                    tool.add();
+                    break;
+            }
+            if (answer.contains("выход")) {
+                break;
+            }
+
+        }
+
 //
 //        Page a = new Page(description,text);
 //        a.display();
@@ -17,12 +42,4 @@ public class Main {
 //        Page с = new Page("А сегодня ничего нового", "Потаюсь написать свой дневник");
 //        с.display();
     }
-
-    private static void menu() {
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-    }
-
 }
